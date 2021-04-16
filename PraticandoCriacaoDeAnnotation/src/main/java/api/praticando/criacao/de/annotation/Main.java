85dc324911508703f7e8b233c4a39dfdf5573964
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-04-16 15:03:54 -0300

    COnfigurado Spring Boot com Spring Web e Criado Hello World com ele

---
+++ PraticandoCriacaoDeAnnotation/src/main/java/api/praticando/criacao/de/annotation/Main.java
@@ -0,0 +1,20 @@
+package api.praticando.criacao.de.annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+
+    @GetMapping("/")
+    public String theFirst(){
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
